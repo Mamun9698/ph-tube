6d2f25035160c658551566a97c25d2cfc86f06ef
Mamun9698 <mamunhowlader149@gmail.com>
2025-12-10 22:28:26 +0600

    second

--- js/category.js
+++ js/category.js
@@ -1,3 +1,21 @@
+//------------------------Timeformating----------------------------
+
+function getTimeString(time){
+  const hour = parseInt(time/3600);
+  let remainingSecond= time % 3600;
+  const minute = parseInt (remainingSecond / 60);
+  remainingSecond=remainingSecond % 60; 
+return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+
+}
+//-------------------------Timeformating-----------------------------
+
+const removeActiveClass = ()=>{
+const buttons = document.getElementsByClassName('category-btn')
+for(let btn of buttons){
+  btn.classList.remove('active')
+}
+}
 
 // 1- fetch, load and show categories
 // create loadCategories
@@ -30,9 +48,6 @@ const cardDemo = {
       },
       "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
     };
-
-
-
 //---------------- carddemo-----------
 
 
@@ -46,20 +61,57 @@ const loadVideos = () =>{
     .then(data=>displayVideos(data.videos))
     // .catch((error) => console.log(error));
     }
+ 
+    const loadCategoriesVideos=(id)=>{
+      // alert(id);
+      fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then(Response=>Response.json())
+    .then((data)=>{
+      // remove active button
+      removeActiveClass();
+      const activeBtn = document.getElementById(`btn-${id}`)
+      activeBtn.classList.add('active');
+      displayVideos(data.category);
+    });
+       
+      
+    }
+    // .catch((error) => console.log(error));
+      
+    
+    
 
  const displayVideos = (videos)=>{
     const videoContainer = document.getElementById('videos'); 
+    videoContainer.classList.remove('grid');
+    videoContainer.innerHTML="";
+    if(videos.length===0){
+      videoContainer.innerHTML = `
+      <div class="min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+      <img src="assets/Icon.png"/> 
+      <h2 class= "text-center text-2xl font-bold">No content here in this Category</h2>
+      </div>
+      `
+      videos.
+      return;
+    }else{
+      videoContainer.classList.add('grid');
+    }
     videos.forEach(video=>{
         console.log(video); 
         const card = document.createElement('div');
         card.classList = 'card  '
         card.innerHTML=
         `
-        <figure class="h-[200px]">
+        <figure class="h-[200px] relative">
         <img
           src=${video.thumbnail}
           class="h-full w-full object-cover"
           alt="Shoes" />
+          ${video.others.posted_date?.length == 0 ? "" : `<span class="absolute text-xs right-2 bottom-2 text-white">${getTimeString(video.others.posted_date)}</span>`
+
+          }
+          
       </figure>
       <div class="px-0 py-2 flex gap-2">
         <div>
@@ -69,7 +121,8 @@ const loadVideos = () =>{
         <h2 class="font-bold">${video.title}</h2>
         <div class='flex items-center gap-2'>
         <p class="text-gray-400">${video.authors[0].profile_name}</p>
-        <img class='w-5' src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>
+        ${video.authors[0].verified===true? `<img class='w-5' src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>`: ""}
+        
         </div>
         </div>
       </div>
@@ -79,15 +132,9 @@ const loadVideos = () =>{
        
     })
  }
-
-    //---------------------------------videocategory end------------------------------------------------------
+ //---------------------------------videocategory end------------------------------------------------------
 
 // create displayCategories
-
-/**
- *  "category_id": "1001",
-    "category": "Music
- */
 
 const displayCatagories = (categories) =>{
 const categoryContainer = document.getElementById('categories')    
@@ -95,11 +142,16 @@ const categoryContainer = document.getElementById('categories')
         // console.log(item)
         // creat a button
 
-        const button = document.createElement('button');
-        button.classList = 'btn';
-        button.innerText= item.category;
+        const buttonContainer = document.createElement('div');
+        buttonContainer.innerHTML=
+        `
+        <button id="btn-${item.category_id}" onclick="loadCategoriesVideos(${item.category_id})" class="btn category-btn">
+        ${item.category}
+        </button>
+        `
+        
         // add button to caegory container
-        categoryContainer.append(button);
+        categoryContainer.append(buttonContainer);
     });
 
 }
---
+++ js/test.js
@@ -0,0 +1,17 @@
+// const isVerified = true;
+// if(isVerified===true){
+// console.log('user is verified');
+
+// }else{
+//     console.log('user is not verified'); 
+// }
+
+
+function getTimeString(time){
+    const hour = parseInt(time/3600);
+    let remainingSecond= time % 3600;
+    const minute = parseInt (remainingSecond / 60);
+    remainingSecond=remainingSecond % 60; 
+ return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+
+}
